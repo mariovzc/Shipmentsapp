c22db148f228c295f193a9bf048f856257d10f0f
¨Mario <mario.vizcainos@gmail.com>
2017-04-09 18:38:14 -0500

    added nil data validation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,6 +2,10 @@ $( document ).ready(function() {
     $( "#search-package" ).submit(function( event ) {
         event.preventDefault();
         $.get( "getPackageByTrackingID/" + $("#search").val() + ".json", function( data ) {
+            if(data === null){
+                alert("Codigo no valido o ya entregado");
+                return;
+            }
             console.log(data);
             $('#trackingid').val($("#search").val());
             $('#address').val();
